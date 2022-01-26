eacb4070d20dfacc73e1cc11d67d0396e3b8199d
DayanMonteiro <dmsmamb@gmail.com>
2022-01-26 18:39:25 -0300

    project

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import { SignInButton } from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
+
+import styles from './styles.module.scss'
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="logo ig.news" />
+
+                <nav>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                     <a className={styles.active}>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                    <a href="">Posts</a>
+                    </ActiveLink>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    )
+}
